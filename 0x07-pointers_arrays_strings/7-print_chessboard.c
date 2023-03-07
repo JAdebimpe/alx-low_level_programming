39c21a76a553282aa75bc65a014064e34cef2379
JAdebimpe <jinaduadebimpe2020@gmail.com>
2023-03-06 19:44:00 -0800

    8-print_diagsums

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * Print_chessboard -Entry point
+ * @array: input
+ * Return: Always 0(Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, n;
+for(i = 0; i < 8; i++)
+{
+for(n = 0; n < 8; n++)
+_putchar(a[i][n]);
+_putchar('\n');
+}
+}
